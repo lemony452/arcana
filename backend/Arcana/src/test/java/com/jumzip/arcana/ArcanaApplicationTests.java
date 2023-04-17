c880409390cb3d79b3c8dbfd8ed31bca2d29586e
jesniey11@gmail.com <jesniey11@gmail.com>
2023-04-17 11:55:01 +0900

    [BE] chore : File tree

---
+++ backend/Arcana/src/test/java/com/jumzip/arcana/ArcanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jumzip.arcana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcanaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
