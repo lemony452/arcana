2e66b9d552bc51dfeb5213a6bf99f9c57e6ce179
lemony452 <gij9808@gmail.com>
2023-04-27 10:33:56 +0900

    [fe] feat: Layout

---
+++ frontend/src/Page/Main/main_style.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+
+// export const GlobalStyle = createGlobalStyle`
+//   display: flex;
+// `;
+
+export const TitleBox = styled.div`
+  align-content: center;
+  flex-direction: column;
+  position: absolute;
+  top: 32vh;
+  left: 3vw;
+  text-align: center;
+  z-index: 1;
+`;
+
+export const Title = styled.div`
+  font-size: 7.5vw;
+  font-weight: bolder;
+`;
+
+export const SubTitle = styled.div`
+  font-size: 1.7vw;
+  font-weight: bold;
+`;
+
+export const StyledCircle = styled.div<{ color: string }>`
+  position: absolute;
+  right: calc(100vw / 3);
+  width: 50vh;
+  height: 100vh;
+  border-radius: 100vh 0 0 100vh;
+  background: ${(props) => props.color || 'beige'};
+`;
+
+export const CardBox = styled.div`
+  position: absolute;
+  right: 0;
+  width: calc(100vw / 3);
+  height: 100vh;
+  filter: drop-shadow(-10px 0px 15px rgba(0, 0, 0, 0.7));
+  /* background: pink; */
+`;
+
+export const StyledCard = styled.img`
+  src: url(imgsrc);
+  width: calc(100vw / 3);
+  height: 100vh;
+  /* object-fit: fill; */
+`;
