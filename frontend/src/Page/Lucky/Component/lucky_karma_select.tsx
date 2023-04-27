0fd58018e60066414999ebf0ee948b7c45f31ad0
Choi Jihun <totolo312@naver.com>
2023-04-27 14:52:40 +0900

    feat: 카드 선택 애니메이션

--- frontend/src/Page/Lucky/Component/lucky_karma_select.tsx
+++ frontend/src/Page/Lucky/Component/lucky_karma_select.tsx
@@ -1,27 +1,76 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as luckySelect from './lucky_karma_select_style';
 import CardB from '../../../Assets/cards/cardb.svg';
 import CardF from '../../../Assets/cards/cardf.svg';
 import Gom from '../../../Assets/cards/gom.svg';
 
 function LuckyKarmaSelect() {
-  const cardList = [1, 2, 3];
+  const [cardState, setCardState] = useState([false, false, false]);
+  const [cardContent, setCardContent] = useState('카드를 골라주세요');
+  const [cardAniState, setCardAniState] = useState([false, false, false]);
+
+  const cardList = [
+    { index: 0, front: CardF, content: '너의 행운 카드는 ㅇㅇㅇ 이야! 아마 너에게 특별한 행운을 가져다 줄걸?' },
+    { index: 1, front: CardF, content: '너의 행운 카드는 ㅇㅇㅇ 이야! 아마 너에게 새로운 시작을 가져다 줄걸?' },
+    { index: 2, front: CardF, content: '너의 행운 카드는 ㅇㅇㅇ 이야! 아마 너에게 놀라운 경험을 가져다 줄걸?' },
+  ];
+
+  const cardClickHandler = (index: number) => {
+    // 카드 리스트 인덱스따라서 카드를 뒤집음
+    if (index === 0) {
+      cardAniState[0] = !cardAniState[0];
+      setCardAniState([cardAniState[0], false, false]);
+    } else if (index === 1) {
+      cardAniState[1] = !cardAniState[1];
+      setCardAniState([false, cardAniState[1], false]);
+    } else {
+      cardAniState[2] = !cardAniState[2];
+      setCardAniState([false, false, cardAniState[2]]);
+    }
+
+    // 지연시간을 주어서 카드 뒤집을때 끊키는 현상 제거
+    setTimeout(() => {
+      if (index === 0) {
+        cardState[0] = !cardState[0];
+        setCardState([cardState[0], false, false]);
+      } else if (index === 1) {
+        cardState[1] = !cardState[1];
+        setCardState([false, cardState[1], false]);
+      } else {
+        cardState[2] = !cardState[2];
+        setCardState([false, false, cardState[2]]);
+      }
+      // 카드 인덱스로 컨텐츠 출력
+      if (cardState[0] === false && cardState[1] === false && cardState[2] === false) {
+        setCardContent('카드를 골라주세요');
+      } else {
+        setCardContent(cardList[index].content);
+      }
+    }, 200);
+  };
+
   return (
     <luckySelect.Body>
       <luckySelect.CardBody>
-        {cardList.map(() => {
-          return (
-            <luckySelect.Card>
-              <img src={CardB} alt="CardB" />
-            </luckySelect.Card>
-          );
-        })}
+        {cardList.map((card) => (
+          <luckySelect.Card
+            onClick={() => {
+              cardClickHandler(card.index);
+            }}
+          >
+            {cardState[card.index] ? (
+              <luckySelect.CardFront src={card.front} alt="CardF" cardAniState={cardAniState[card.index]} />
+            ) : (
+              <luckySelect.CardBack src={CardB} alt="CardB" cardAniState={cardAniState[card.index]} />
+            )}
+          </luckySelect.Card>
+        ))}
       </luckySelect.CardBody>
       <luckySelect.Dialog>
         <luckySelect.Name>농담곰</luckySelect.Name>
         <luckySelect.Content>
           <hr />
-          너의 행운 카드는 ㅇㅇㅇ 이야! 아마 너에게 특별한 행운을 가져다 줄걸?
+          {cardContent}
         </luckySelect.Content>
       </luckySelect.Dialog>
       <luckySelect.Gom>
--- frontend/src/Page/Lucky/Component/lucky_karma_select_style.tsx
+++ frontend/src/Page/Lucky/Component/lucky_karma_select_style.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 export const Body = styled.div`
   background-color: #faf7f7;
@@ -11,14 +11,44 @@ export const CardBody = styled.div`
   justify-content: center;
   align-items: center;
   height: 67%;
+  transform-style: preserve-3d;
+`;
+
+const CardAnimationS = keyframes`
+  0% {
+    transform: rotateY(-90deg)
+  }
+  100% {
+    transform: rotateY(0deg)
+  }
+`;
+
+const CardAnimationE = keyframes`
+  0% {
+    transform: rotateY(0deg)
+  }
+  100% {
+    transform: rotateY(-90deg)
+  }
 `;
 
 export const Card = styled.div`
   padding: 30px;
-  > img {
-    width: auto;
-    max-height: 350px;
-  }
+`;
+
+export const CardBack = styled.img<{ cardAniState: boolean }>`
+  width: auto;
+  max-height: 350px;
+  // 매개변수를 받아서 카드를 뒤집는 상태를 조정
+  animation-name: ${(props) => (props.cardAniState ? CardAnimationE : CardAnimationS)};
+  animation-duration: 0.3s;
+`;
+
+export const CardFront = styled.img<{ cardAniState: boolean }>`
+  width: auto;
+  max-height: 350px;
+  animation-name: ${(props) => (props.cardAniState ? CardAnimationS : CardAnimationE)};
+  animation-duration: 0.3s;
 `;
 
 export const Dialog = styled.div`
