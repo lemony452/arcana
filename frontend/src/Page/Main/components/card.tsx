d05e467480724242adaa405957be85ac96820c3f
lemony452 <gij9808@gmail.com>
2023-04-27 23:54:54 +0900

    [fe] feat: Main 운세종류 swipe 구현

--- frontend/src/Page/Main/components/card.tsx
+++ frontend/src/Page/Main/components/card.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Swiper, SwiperSlide } from 'swiper/react';
+// eslint-disable-next-line import/no-unresolved
+import 'swiper/css';
+// eslint-disable-next-line import/no-unresolved
+import 'swiper/css/navigation';
 import { StyledCard } from '../main_style';
-import CardSrc from '../../../Assets/etc/card0.png';
+import Card0 from '../../../Assets/etc/card0.png';
+import Card1 from '../../../Assets/etc/card1.png';
+import Card2 from '../../../Assets/etc/card2.png';
 
 function Card() {
-  return <StyledCard src={CardSrc} />;
+  const navigate = useNavigate();
+
+  const MoveCeltic = () => {
+    navigate('/celtic');
+  };
+
+  return (
+    <Swiper modules={[Navigation, Pagination, Scrollbar, A11y]} navigation pagination={{ clickable: true }}>
+      <SwiperSlide>
+        <StyledCard onClick={MoveCeltic} src={Card0} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <StyledCard onClick={MoveCeltic} src={Card1} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <StyledCard onClick={MoveCeltic} src={Card2} />
+      </SwiperSlide>
+    </Swiper>
+  );
 }
 
 export default Card;
