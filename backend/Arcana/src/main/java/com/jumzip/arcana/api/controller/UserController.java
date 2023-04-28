3160fe6ec0e1edd70ad97fe08ddbe4593de2462a
jesniey11@gmail.com <jesniey11@gmail.com>
2023-04-28 16:51:15 +0900

    test : Jenkins build test with Gradle

--- backend/Arcana/src/main/java/com/jumzip/arcana/api/controller/UserController.java
+++ backend/Arcana/src/main/java/com/jumzip/arcana/api/controller/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private UserService userService;
-    private UserService tmp;
 
     /* Firebase  */
 
