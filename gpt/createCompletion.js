6430c23e982ca1cd9b0ef682d9998c5f0343632b
SangHa Jeon <aeoragy@gmail.com>
2023-05-01 17:37:37 +0900

    fix : GPT Model
    학습 모델 변경

--- gpt/createCompletion.js
+++ gpt/createCompletion.js
@@ -3,9 +3,9 @@ import { openai } from './api.js'
 async function createCompletion() {
   try {
     const response = await openai.createCompletion({
-      model: 'davinci:ft-personal-2023-04-28-02-17-42',
-      prompt: '[카드목록][Three of Cups, The Sun(Reverse), Two of Pentacles, The High Priestess, King of Swords(Reverse), Nine of Wands, Seven of Cups(Reverse), The Empress, The Tower(Reverse), Wheel of Fortune] 카드가 있다. [방식] celtic-cross. 학업과 관련된 점을 보고 싶다. 1, 2번째 카드의 결과만 응답한다. [질문] 중요한 시험을 앞두고 있습니다. 잘 볼 수 있을까요?',
-      max_tokens: 300
+      model: 'davinci:ft-personal-2023-05-01-08-24-11',
+      prompt: '[카드목록][The Sun, The Tower, The Empress, The Wheel of Fortune(Reverse), The Hierophant, Nine of Cups, The Moon, The Magician, Two of Wands(Reverse), Page of Pentacles] 카드가 있다. [방식] celtic-cross. 학업과 관련된 점을 보고 싶다. 1, 2번째 카드의 결과만 응답한다. [질문] 내일 시험인데 잘 볼 수 있을까?',
+      max_tokens: 220
     })
     if (response.data) {
       console.log('choices: ', response.data.choices)
