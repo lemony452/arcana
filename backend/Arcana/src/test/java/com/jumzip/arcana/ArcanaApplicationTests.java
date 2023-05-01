cd987cc71053eefa2a71c3658c83a3b69c30ad9b
jesniey11@gmail.com <jesniey11@gmail.com>
2023-05-01 17:12:01 +0900

    fix : unused annotation remove

--- backend/Arcana/src/test/java/com/jumzip/arcana/ArcanaApplicationTests.java
+++ backend/Arcana/src/test/java/com/jumzip/arcana/ArcanaApplicationTests.java
@@ -3,7 +3,7 @@ package com.jumzip.arcana;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ArcanaApplicationTests {
 
     @Test
