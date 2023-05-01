6b9ef60a9bb84eb2cf9f76f12b71e743a3468c16
Lee_Jungyun <leejy96kr@naver.com>
2023-05-01 13:05:12 +0900

    fix: celtic spread 페이지 정리

--- frontend/src/Page/Celtic/celtic_spread_idx.tsx
+++ frontend/src/Page/Celtic/celtic_spread_idx.tsx
@@ -1,21 +1,109 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
 import * as layer from './celtic_spread_style';
 import Celtic from './celtic_spread';
-import { SpreadBtn } from '../Common/common_style';
+import { SpreadBtn, CardArea, DefaultCard, ChatArea, SpreadModal } from '../Common/common_style';
 
 function CelticSpread() {
-  const navigate = useNavigate();
-  const MoveCelticDetail = () => {
-    navigate('/celtic/detail');
+  // const navigate = useNavigate();
+  const [index, setIndex] = useState(0);
+  const onNext = (e: any) => {
+    return setIndex(index + 1);
   };
+  console.log(index);
+  // const MoveCelticDetail = () => {
+  //   navigate('/celtic/detail');
+  // };
 
-  return (
-    <layer.MainBox>
-      <Celtic />
-      <SpreadBtn onClick={MoveCelticDetail}>해석보기</SpreadBtn>
-    </layer.MainBox>
-  );
+  if (index === 0) {
+    return (
+      <layer.MainBox>
+        <Celtic />
+        <SpreadBtn onClick={onNext}>해석보기</SpreadBtn>
+      </layer.MainBox>
+    );
+  }
+  if (index === 1) {
+    return (
+      <>
+        <CardArea>
+          <DefaultCard>1</DefaultCard>
+          <DefaultCard>2</DefaultCard>
+          <SpreadModal>
+            <Celtic />
+          </SpreadModal>
+        </CardArea>
+        <ChatArea>
+          <SpreadBtn onClick={onNext}>다음</SpreadBtn>
+        </ChatArea>
+      </>
+    );
+  }
+  if (index === 2) {
+    return (
+      <>
+        <CardArea>
+          <DefaultCard>3</DefaultCard>
+          <DefaultCard>4</DefaultCard>
+          <SpreadModal>
+            <Celtic />
+          </SpreadModal>
+        </CardArea>
+        <ChatArea>
+          <SpreadBtn onClick={onNext}>다음</SpreadBtn>
+        </ChatArea>
+      </>
+    );
+  }
+  if (index === 3) {
+    return (
+      <>
+        <CardArea>
+          <DefaultCard>5</DefaultCard>
+          <DefaultCard>6</DefaultCard>
+          <SpreadModal>
+            <Celtic />
+          </SpreadModal>
+        </CardArea>
+        <ChatArea>
+          <SpreadBtn onClick={onNext}>다음</SpreadBtn>
+        </ChatArea>
+      </>
+    );
+  }
+  if (index === 4) {
+    return (
+      <>
+        <CardArea>
+          <DefaultCard>7</DefaultCard>
+          <DefaultCard>8</DefaultCard>
+          <DefaultCard>9</DefaultCard>
+          <SpreadModal>
+            <Celtic />
+          </SpreadModal>
+        </CardArea>
+        <ChatArea>
+          <SpreadBtn onClick={onNext}>다음</SpreadBtn>
+        </ChatArea>
+      </>
+    );
+  }
+  if (index === 5) {
+    return (
+      <>
+        <CardArea>
+          <DefaultCard>10</DefaultCard>
+          <SpreadModal>
+            <Celtic />
+          </SpreadModal>
+        </CardArea>
+        <ChatArea>
+          <SpreadBtn>럭키!</SpreadBtn>
+        </ChatArea>
+      </>
+    );
+  }
+  return null;
 }
 
 export default CelticSpread;
