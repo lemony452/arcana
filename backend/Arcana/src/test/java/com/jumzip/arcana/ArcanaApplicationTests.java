718cc7f169a005c9f79ba62522b61b5b33c69e6d
jesniey11@gmail.com <jesniey11@gmail.com>
2023-05-02 18:00:28 +0900

    test : build docker test

--- backend/Arcana/src/test/java/com/jumzip/arcana/ArcanaApplicationTests.java
+++ backend/Arcana/src/test/java/com/jumzip/arcana/ArcanaApplicationTests.java
@@ -3,7 +3,8 @@ package com.jumzip.arcana;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+// Test 미사용으로 인한 properties 추가
+@SpringBootTest(properties = "spring.profiles.active:prod")
 class ArcanaApplicationTests {
 
     @Test
