7f3aa1847e4b0f11a9512cdb80fe1df7c0ffe5cc
jesniey11 <jesniey11@gmail.com>
2023-05-02 22:22:05 +0900

    test : build test

--- backend/Arcana/src/test/java/com/jumzip/arcana/ArcanaApplicationTests.java
+++ backend/Arcana/src/test/java/com/jumzip/arcana/ArcanaApplicationTests.java
@@ -3,8 +3,8 @@ package com.jumzip.arcana;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// Test 미사용으로 인한 properties 추가
-@SpringBootTest(properties = "spring.profiles.active:prod")
+// Test 미사용으로 인한 주석처리
+//@SpringBootTest
 class ArcanaApplicationTests {
 
     @Test
