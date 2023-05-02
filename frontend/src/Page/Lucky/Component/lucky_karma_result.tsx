b7cb5d53b2f272ccc0f5c49eb0e9399cd0c9aca6
Choi Jihun <totolo312@naver.com>
2023-05-02 17:49:02 +0900

    feat: 공유하기 기능 추가

--- frontend/src/Page/Lucky/Component/lucky_karma_result.tsx
+++ frontend/src/Page/Lucky/Component/lucky_karma_result.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import {
   EmailShareButton,
   EmailIcon,
@@ -40,7 +41,7 @@ import {
   WorkplaceShareButton,
   WorkplaceIcon,
 } from 'react-share';
-
+import KakaoIcon from '../../../Assets/etc/icon-kakao.png';
 import * as luckResult from './lucky_karma_result_style';
 
 interface LuckyKarmaResultType {
@@ -54,16 +55,89 @@ interface LuckyKarmaResultType {
 }
 
 function LuckyKarmaResult({ selectCard, cardList }: LuckyKarmaResultType) {
+  // const ACCESS_TOKEN =
+  //   'IGQVJVaG56V0FpdXNoaVFrcG5YcGp3ZAHEySUo4ZAkhIeTN2NjBtNUdRWEh1dl8tVTBIUHJiRmQ3MmFTNVZAWcVJFLW43eGc4Y3dWLWhESGFMU0FJRUNpSjF6b2kzU2RPd09GSzUzQVZAQTnFsX1c4S2RXRQZDZD';
+  // const MEDIA_ENDPOINT = 'https://graph.instagram.com/me/media';
+  // const PUBLISH_ENDPOINT = 'https://graph.instagram.com/me/media_publish';
+
+  // const sendInstagram = async () => {
+  //   try {
+  //     // Upload image to Instagram
+  //     const mediaResponse = await axios.post(`${MEDIA_ENDPOINT}`, {
+  //       access_token: ACCESS_TOKEN,
+  //       image_url:
+  //         'https://png.pngtree.com/png-vector/20210326/ourmid/pngtree-sing-moon-tarot-symbol-png-image_3139433.jpg',
+  //       caption: cardList[selectCard].result,
+  //     });
+  //     const mediaId = mediaResponse.data.id;
+
+  //     // Share media to your feed
+  //     const publishResponse = await axios.post(`${PUBLISH_ENDPOINT}`, {
+  //       access_token: ACCESS_TOKEN,
+  //       media_id: mediaId,
+  //       caption: cardList[selectCard].result,
+  //       media_url: 'http://localhost:3000',
+  //       media_type: 'IMAGE', // Change to "IMAGE" if sharing an image instead
+  //     });
+
+  //     console.log('Successfully shared to Instagram!');
+  //   } catch (error) {
+  //     console.error('Error sharing to Instagram:', error);
+  //   }
+  // };
+
+  const sendKakao = () => {
+    window.Kakao.Link.sendDefault({
+      objectType: 'feed',
+      content: {
+        title: '내 럭키카드는 결과는?', // 공유 타이틀
+        description: cardList[selectCard].result, // 공유 내용
+        imageUrl:
+          'https://png.pngtree.com/png-vector/20210326/ourmid/pngtree-sing-moon-tarot-symbol-png-image_3139433.jpg', // 공유 이미지
+        link: {
+          webUrl: 'http://localhost:3000', // 공유 링크
+        },
+      },
+      buttons: [
+        {
+          title: '내 운세 점치러 가기', // 공유 초대 링크 타이틀
+          link: {
+            webUrl: 'http://localhost:3000', // 공유 초대 링크
+          },
+        },
+      ],
+    });
+  };
+
   return (
     <luckResult.Body>
       <luckResult.CardBody>
         <luckResult.Card>
-          <luckResult.CardFront src={cardList[selectCard].front} alt="CardF" />
+          <luckResult.CardFront style={{ marginInline: '10px' }} src={cardList[selectCard].front} alt="CardF" />
           <p>{cardList[selectCard].result}</p>
-          <EmailIcon size={32} round />
-          <FacebookIcon size={32} round />
-          <TwitterIcon size={32} round />
-          <LineIcon size={32} round />
+          <FacebookShareButton style={{ marginInline: '10px' }} url={window.location.href}>
+            <FacebookIcon size={32} round borderRadius={24} />
+          </FacebookShareButton>
+          <TwitterShareButton
+            style={{ marginInline: '10px' }}
+            title={cardList[selectCard].result}
+            url="http://localhost:3000"
+          >
+            <TwitterIcon size={32} round borderRadius={24} />
+          </TwitterShareButton>
+          <TelegramShareButton
+            style={{ marginInline: '10px' }}
+            title={cardList[selectCard].result}
+            url="http://localhost:3000"
+          >
+            <TelegramIcon size={32} round borderRadius={24} />
+          </TelegramShareButton>
+          <luckResult.Button onClick={sendKakao} style={{ marginInline: '10px' }}>
+            <img src={KakaoIcon} alt="kakaoicon" />
+          </luckResult.Button>
+          {/* <luckResult.Button onClick={sendInstagram} style={{ marginInline: '10px' }}>
+            <img src={KakaoIcon} alt="kakaoicon" />
+          </luckResult.Button> */}
         </luckResult.Card>
       </luckResult.CardBody>
     </luckResult.Body>
