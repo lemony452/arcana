227ac3f5eaa8eaa2c49768523512dd7f14f50492
Choi Jihun <totolo312@naver.com>
2023-05-02 09:02:31 +0900

    feat: 공유하기 라이브러리

--- frontend/src/Page/Lucky/Component/lucky_karma_result.tsx
+++ frontend/src/Page/Lucky/Component/lucky_karma_result.tsx
@@ -1,4 +1,46 @@
 import React from 'react';
+import {
+  EmailShareButton,
+  EmailIcon,
+  FacebookShareButton,
+  FacebookIcon,
+  FacebookMessengerIcon,
+  HatenaShareButton,
+  HatenaIcon,
+  InstapaperShareButton,
+  InstapaperIcon,
+  LineShareButton,
+  LineIcon,
+  LinkedinShareButton,
+  LinkedinIcon,
+  LivejournalShareButton,
+  LivejournalIcon,
+  MailruShareButton,
+  MailruIcon,
+  OKShareButton,
+  OKIcon,
+  PinterestShareButton,
+  PinterestIcon,
+  PocketShareButton,
+  PocketIcon,
+  RedditShareButton,
+  RedditIcon,
+  TelegramShareButton,
+  TelegramIcon,
+  TumblrShareButton,
+  TumblrIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  ViberShareButton,
+  ViberIcon,
+  VKShareButton,
+  VKIcon,
+  WhatsappShareButton,
+  WhatsappIcon,
+  WorkplaceShareButton,
+  WorkplaceIcon,
+} from 'react-share';
+
 import * as luckResult from './lucky_karma_result_style';
 
 interface LuckyKarmaResultType {
@@ -18,6 +60,10 @@ function LuckyKarmaResult({ selectCard, cardList }: LuckyKarmaResultType) {
         <luckResult.Card>
           <luckResult.CardFront src={cardList[selectCard].front} alt="CardF" />
           <p>{cardList[selectCard].result}</p>
+          <EmailIcon size={32} round />
+          <FacebookIcon size={32} round />
+          <TwitterIcon size={32} round />
+          <LineIcon size={32} round />
         </luckResult.Card>
       </luckResult.CardBody>
     </luckResult.Body>
