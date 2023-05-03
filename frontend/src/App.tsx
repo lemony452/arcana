d31aefa5f5a8b22a9d72b11ed3f6e478ce236098
Lee_Jungyun <leejy96kr@naver.com>
2023-05-03 14:47:13 +0900

    feat: select 관련 app.tsx에서 경로 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,12 @@ import './App.css';
 import Main from './Page/Main';
 import Celtic from './Page/Celtic';
 import Time from './Page/Time';
+import Select from './Page/Select';
 import CelticSpread from './Page/Celtic/celtic_spread_idx';
 import MonthSpread from './Page/Time/Month/month_spread_idx';
 import YearSpread from './Page/Time/Year/year_spread_idx';
+import SelectTwo from './Page/Select/Two/two_spread_idx';
+import SelectThree from './Page/Select/Three/three_spread_idx';
 import LuckyPage from './Page/Lucky/lucky_page';
 import CelticDetail from './Page/Celtic/detail';
 
@@ -26,6 +29,9 @@ function App() {
         <Route path="/time" element={<Time />} />
         <Route path="/time/month" element={<MonthSpread />} />
         <Route path="/time/year" element={<YearSpread />} />
+        <Route path="/select" element={<Select />} />
+        <Route path="/select/two" element={<SelectTwo />} />
+        <Route path="/select/three" element={<SelectThree />} />
         <Route path="/lucky" element={<LuckyPage />} />
         <Route path="/celtic/detail" element={<CelticDetail />} />
       </Routes>
