84ab8ffa4559aa3badada623d0df7042b6199433
Lee_Jungyun <leejy96kr@naver.com>
2023-05-04 09:39:17 +0900

    fix: select->instant로 이름변경 및 폴더 움직임

--- frontend/src/Page/Instant/index.tsx
+++ frontend/src/Page/Instant/index.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { SpreadBtn } from '../Common/common_style';
 
 function Instant() {
-  return <>인스턴트 페이지입니다.</>;
+  const navigate = useNavigate();
+  const MoveTwo = () => {
+    navigate('/instant/two');
+  };
+  const MoveThree = () => {
+    navigate('/instant/three');
+  };
+
+  return (
+    <>
+      <div>let 질문을 받았다 가정하자</div>
+      <SpreadBtn onClick={MoveTwo}>2가지 고민</SpreadBtn>
+      <SpreadBtn onClick={MoveThree}>3가지 고민</SpreadBtn>
+    </>
+  );
 }
 
 export default Instant;
