ff6e9e50edbd9945022f3d729572eebd17009296
Lee Jungyun <leejy96kr@naver.com>
2023-05-07 11:18:40 +0900

    reverting wrong commit

--- frontend/src/API.tsx
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import React from 'react';
-
-export const API = axios.create({
-  baseURL: `SERVER_ADDRESS`, // 기본 서버 주소
-  headers: {
-    access_token: 'access_token',
-  },
-});
