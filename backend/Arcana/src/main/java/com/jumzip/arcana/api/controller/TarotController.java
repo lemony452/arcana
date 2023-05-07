28a860588fd9e34fc12005c981bfa03b44972183
jesniey11 <jesniey11@gmail.com>
2023-05-08 04:29:19 +0900

    feat : swagger setting

--- backend/Arcana/src/main/java/com/jumzip/arcana/api/controller/TarotController.java
+++ backend/Arcana/src/main/java/com/jumzip/arcana/api/controller/TarotController.java
@@ -7,21 +7,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-//@Api 스웨거어디갔어;;
 @RestController
 @RequestMapping("api/tarot/")
 public class TarotController {
 
     private TarotService tarotService;
 
-    // 1개의 인스턴트 api에 선택 int 넣어서 부르기
-    @GetMapping("instant")
+    // 선택지1. 1개의 인스턴트 api에 선택 int 넣어서 부르기
+    @GetMapping("instant/{}")
     public Card[] GetInstant(@RequestParam int cardNum) {
         Card[] cardlist = tarotService.getInstantResult(cardNum);
 
         return cardlist;
     }
 
+    // 선택지2. api 2개
     @GetMapping("instant/two")
     public Card[] GetTwoWay() {
         Card[] cardlist = new Card[3];
---
+++ backend/Arcana/src/main/java/com/jumzip/arcana/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.jumzip.arcana.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+
+public class SwaggerConfig {
+
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.OAS_30)
+//                .useDefaultResponseMessages(false)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.example.springswagger.controller"))
+//                .paths(PathSelectors.any())
+//                .build()
+//                .apiInfo(apiInfo());
+//    }
+//
+//    private ApiInfo apiInfo() {
+//        return new ApiInfoBuilder()
+//                .title("Practice Swagger")
+//                .description("practice swagger config")
+//                .version("1.0")
+//                .build();
+//    }
+}
