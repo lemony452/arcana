8bef89884651e5d15485a9880709d75643866beb
강인주 <gij452@kakao.com>
2023-05-08 15:45:27 +0900

    feat : 마이페이지 사이드 바 레이아웃 구현

--- frontend/src/Page/Main/index.tsx
+++ frontend/src/Page/Main/index.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import { TitleBox, Title, SubTitle, StyledCircle, CardBox } from './main_style';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { TitleBox, Title, SubTitle, StyledCircle, CardBox, SideBtn } from './main_style';
 import Card from './components/card';
 import Character from './components/character';
 import { useCardStore, useHoverStore } from '../../Store/Main/main';
+import SideBtnImg from '../../Assets/etc/sideBtn.png';
+import SideBar from '../Mypage';
 
 function Main() {
+  const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleSide = () => {
+    setIsOpen(true);
+  };
+
   const { cardOrder } = useCardStore();
   const { hover } = useHoverStore();
   let color = 'pink';
@@ -13,10 +22,13 @@ function Main() {
   } else if (cardOrder === 'instant') {
     color = 'green';
   }
+
   console.log(cardOrder);
   console.log(hover);
   return (
     <div style={{ position: 'relative' }}>
+      <SideBtn src={SideBtnImg} onClick={toggleSide} />
+      <SideBar isOpen={isOpen} setIsOpen={setIsOpen} />
       <TitleBox>
         <Title>ARCANA</Title>
         <SubTitle>동물 친구들의 타로 서비스 아르카나</SubTitle>
@@ -24,7 +36,7 @@ function Main() {
       <StyledCircle color={color} />
       <Character />
       <CardBox>
-        <Card />
+        <Card isOpen={isOpen} />
       </CardBox>
     </div>
   );
