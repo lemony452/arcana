1390661d0d5b3568cd11d40e2f7d9d847c6eae84
lemony452 <gij9808@gmail.com>
2023-05-09 09:55:52 +0900

    feat : 유저 닉네임, 토큰 상태관리 변수생성

--- frontend/src/Store/User/info.tsx
+++ frontend/src/Store/User/info.tsx
@@ -3,17 +3,23 @@ import { create } from 'zustand';
 interface InfoState {
   nickname: string;
   token: number;
+  isLogin: boolean;
   setNickname: (name: string) => void;
   setTocken: (t: number) => void;
+  setIsLogin: (login: boolean) => void;
 }
 
 export const useInfoStore = create<InfoState>((set) => ({
   nickname: '',
   token: 0,
+  isLogin: false,
   setNickname: (name) => {
     set(() => ({ nickname: name }));
   },
   setTocken: (t) => {
     set(() => ({ token: t }));
+  },
+  setIsLogin: (login) => {
+    set(() => ({ isLogin: login }));
   },
 }));
