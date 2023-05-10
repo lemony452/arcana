2685d48c84a14beac7fd5621d9ae5ba18db10111
Lee_Jungyun <leejy96kr@naver.com>
2023-05-10 11:11:11 +0900

    fix: time카드재배치

--- frontend/src/Page/Time/Year/year_spread_num.tsx
+++ frontend/src/Page/Time/Year/year_spread_num.tsx
@@ -5,24 +5,21 @@ function YearSpread() {
   return (
     <layer.TimeBox>
       <layer.TimePartOne>
-        <layer.YearCard>13</layer.YearCard>
-      </layer.TimePartOne>
-      <layer.TimePartTwo>
         <layer.YearCard>1</layer.YearCard>
         <layer.YearCard>2</layer.YearCard>
         <layer.YearCard>3</layer.YearCard>
         <layer.YearCard>4</layer.YearCard>
         <layer.YearCard>5</layer.YearCard>
         <layer.YearCard>6</layer.YearCard>
-      </layer.TimePartTwo>
-      <layer.TimePartThree>
+      </layer.TimePartOne>
+      <layer.TimePartTwo>
         <layer.YearCard>7</layer.YearCard>
         <layer.YearCard>8</layer.YearCard>
         <layer.YearCard>9</layer.YearCard>
         <layer.YearCard>10</layer.YearCard>
         <layer.YearCard>11</layer.YearCard>
         <layer.YearCard>12</layer.YearCard>
-      </layer.TimePartThree>
+      </layer.TimePartTwo>
     </layer.TimeBox>
   );
 }
