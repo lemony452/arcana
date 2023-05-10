53a8682e50f00ae801e727224aa33510e4efadf1
Lee_Jungyun <leejy96kr@naver.com>
2023-05-10 17:15:28 +0900

    add: quiztimer

--- frontend/src/Page/Quiz/index.tsx
+++ frontend/src/Page/Quiz/index.tsx
@@ -1,19 +1,135 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import * as quizStyle from './quiz_style';
 
 function Quiz() {
-  return (
-    <quizStyle.FullArea>
-      <quizStyle.LeftArea>
-        <quizStyle.CharacterDialog>오늘도 완주를 향해 화이팅!</quizStyle.CharacterDialog>
-        <quizStyle.CharacterArea>hi</quizStyle.CharacterArea>
-      </quizStyle.LeftArea>
-      <quizStyle.RightArea>
-        <quizStyle.TimerArea>존야</quizStyle.TimerArea>
-        <quizStyle.PeopleArea>인구수가 꽉 찼습니다.</quizStyle.PeopleArea>
-      </quizStyle.RightArea>
-    </quizStyle.FullArea>
-  );
+  const navigate = useNavigate();
+
+  // quiz 풀이시간 카운트 다운
+  const MINUTES_IN_MS = 0;
+  const INTERVAL = 1000;
+  const [timeLeft, setTimeLeft] = useState<number>(MINUTES_IN_MS);
+  const second = String(Math.floor((timeLeft / 1000) % 60)).padStart(2, '0');
+  // const [timeOut, setTimeOut] = useState(false);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft((prevTime) => prevTime - INTERVAL);
+    }, INTERVAL);
+
+    if (timeLeft <= 0) {
+      clearInterval(timer);
+      console.log('타이머가 종료되었습니다.');
+    }
+    return () => {
+      clearInterval(timer);
+    };
+  }, [timeLeft]);
+
+  // 문제 번호 증가 및 시간 초기화.
+  const [index, setIndex] = useState(0);
+  const onNext = () => {
+    return [setIndex(index + 1), setTimeLeft(MINUTES_IN_MS + 10 * 1000)];
+  };
+  console.log('qustion', index);
+
+  // 정답체크 state.
+  const [check, setCheck] = useState();
+  const onCheck = () => {
+    return [setTimeLeft(0)];
+  };
+
+  // 문제 정답을 세는 state.
+  const [correct, setCorrect] = useState(0);
+  const getRight = () => {
+    return setCorrect(correct + 1);
+  };
+  console.log('correct', correct);
+
+  // 시작페이지
+  if (index === 0) {
+    return (
+      <quizStyle.FullArea>
+        <quizStyle.LeftArea>
+          <quizStyle.CharacterDialog>오늘도 완주를 향해 화이팅!</quizStyle.CharacterDialog>
+          {/* 아래 onNext는 백엔드 서버에서 시간 받으면 자동으로 실행되게끔 구현해두어여함 */}
+          <quizStyle.CharacterArea onClick={onNext}>일단 누르면 다음 문제로</quizStyle.CharacterArea>
+        </quizStyle.LeftArea>
+        <quizStyle.RightArea>
+          <quizStyle.TimerArea>존야</quizStyle.TimerArea>
+          <quizStyle.PeopleArea>인구수가 꽉 찼습니다.</quizStyle.PeopleArea>
+        </quizStyle.RightArea>
+      </quizStyle.FullArea>
+    );
+  }
+  if (index === 1) {
+    if (timeLeft !== 0) {
+      return (
+        <quizStyle.FullArea>
+          <quizStyle.LeftCard>
+            <quizStyle.CardArea />
+          </quizStyle.LeftCard>
+          <quizStyle.RightArea>
+            <quizStyle.TimerDivide>{second}</quizStyle.TimerDivide>
+            <quizStyle.TimerDivide>문제 만들기 너어어어어무우우우우 귀찮아요!</quizStyle.TimerDivide>
+            <quizStyle.QuestBox>
+              <quizStyle.QuestArea onClick={onCheck}>A</quizStyle.QuestArea>
+              <quizStyle.QuestArea onClick={onCheck}>B</quizStyle.QuestArea>
+              <quizStyle.QuestArea onClick={onCheck}>C</quizStyle.QuestArea>
+              <quizStyle.QuestArea onClick={onCheck}>D</quizStyle.QuestArea>
+            </quizStyle.QuestBox>
+          </quizStyle.RightArea>
+        </quizStyle.FullArea>
+      );
+    }
+    return (
+      <quizStyle.FullArea>
+        <quizStyle.LeftArea>
+          <quizStyle.CharacterDialog>오늘도 완주를 향해 화이팅! {index}</quizStyle.CharacterDialog>
+          <quizStyle.CharacterArea onClick={onNext}>일단 누르면 다음 문제로</quizStyle.CharacterArea>
+        </quizStyle.LeftArea>
+        <quizStyle.RightArea>
+          <quizStyle.TimerArea>존야</quizStyle.TimerArea>
+          <quizStyle.PeopleArea>인구수가 꽉 찼습니다.</quizStyle.PeopleArea>
+        </quizStyle.RightArea>
+      </quizStyle.FullArea>
+    );
+  }
+  if (index === 2) {
+    if (timeLeft !== 0) {
+      return (
+        <quizStyle.FullArea>
+          <quizStyle.LeftCard>
+            <quizStyle.CardArea />
+          </quizStyle.LeftCard>
+          <quizStyle.RightArea>
+            <quizStyle.TimerDivide>{second}</quizStyle.TimerDivide>
+            <quizStyle.TimerDivide>문제 만들기 너어어어어무우우우우 귀찮아요!</quizStyle.TimerDivide>
+            <quizStyle.QuestBox>
+              <quizStyle.QuestArea onClick={onCheck}>A</quizStyle.QuestArea>
+              <quizStyle.QuestArea onClick={onCheck}>B</quizStyle.QuestArea>
+              <quizStyle.QuestArea onClick={onCheck}>C</quizStyle.QuestArea>
+              <quizStyle.QuestArea onClick={onCheck}>D</quizStyle.QuestArea>
+            </quizStyle.QuestBox>
+          </quizStyle.RightArea>
+        </quizStyle.FullArea>
+      );
+    }
+    return (
+      <quizStyle.FullArea>
+        <quizStyle.LeftArea>
+          <quizStyle.CharacterDialog>오늘도 완주를 향해 화이팅! {index}</quizStyle.CharacterDialog>
+          <quizStyle.CharacterArea onClick={onNext}>일단 누르면 다음 문제로</quizStyle.CharacterArea>
+        </quizStyle.LeftArea>
+        <quizStyle.RightArea>
+          <quizStyle.TimerArea>존야</quizStyle.TimerArea>
+          <quizStyle.PeopleArea>인구수가 꽉 찼습니다.</quizStyle.PeopleArea>
+        </quizStyle.RightArea>
+      </quizStyle.FullArea>
+    );
+  }
+
+  return null;
 }
 
 export default Quiz;
--- frontend/src/Page/Quiz/quiz_style.tsx
+++ frontend/src/Page/Quiz/quiz_style.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const FullArea = styled.div`
   width: 100vw;
   height: 100vh;
-  background-color: purple;
+  background-color: #5438a9;
   display: flex;
 `;
 
@@ -11,6 +11,12 @@ export const LeftArea = styled.div`
   width: 50%;
   height: 100%;
   background-color: red;
+`;
+
+export const LeftCard = styled(LeftArea)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const RightArea = styled.div`
@@ -41,4 +47,30 @@ export const PeopleArea = styled.div`
   width: 100%;
   height: 50%;
   background-color: navy;
+`;
+
+export const CardArea = styled.div`
+  height: 80%;
+  aspect-ratio: 3 / 5;
+  background-color: gold;
+  display: flex;
+`;
+
+export const QuestBox = styled(PeopleArea)`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const QuestArea = styled.div`
+  height: 45%;
+  width: 45%;
+  background-color: teal;
+`;
+
+export const TimerDivide = styled.div`
+  width: 100%;
+  height: 25%;
+  background-color: skyblue;
 `;
