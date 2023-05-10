959f13b750e76bf849a5ec13270df16468f029cf
Lee_Jungyun <leejy96kr@naver.com>
2023-05-10 13:19:11 +0900

    add: quiz인덱스 구역 나눔

--- frontend/src/Page/Quiz/index.tsx
+++ frontend/src/Page/Quiz/index.tsx
@@ -4,8 +4,14 @@ import * as quizStyle from './quiz_style';
 function Quiz() {
   return (
     <quizStyle.FullArea>
-      <quizStyle.LeftArea />
-      <quizStyle.RightArea />
+      <quizStyle.LeftArea>
+        <quizStyle.CharacterDialog>오늘도 완주를 향해 화이팅!</quizStyle.CharacterDialog>
+        <quizStyle.CharacterArea>hi</quizStyle.CharacterArea>
+      </quizStyle.LeftArea>
+      <quizStyle.RightArea>
+        <quizStyle.TimerArea>존야</quizStyle.TimerArea>
+        <quizStyle.PeopleArea>인구수가 꽉 찼습니다.</quizStyle.PeopleArea>
+      </quizStyle.RightArea>
     </quizStyle.FullArea>
   );
 }
