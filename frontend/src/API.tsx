b990300a091f2ebd86e9c9e2862c30f632a6cb23
lemony452 <gij9808@gmail.com>
2023-05-10 13:49:21 +0900

    fix : API content_type 추가

--- frontend/src/API.tsx
+++ frontend/src/API.tsx
@@ -4,7 +4,8 @@ import React from 'react';
 export const API = axios.create({
   baseURL: `http://k8d107.p.ssafy.io:18081`, // 기본 서버 주소
   // baseURL: `http://localhost:3000`, // 기본 서버 주소
-  // headers: {
-  //   access_token: null,
-  // },
+  headers: {
+    'Content-Type': 'application/json',
+    // access_token: null,
+  },
 });
