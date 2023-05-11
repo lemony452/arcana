9a1c992423e2c941a8841f6185b2c1944bad41d1
SangHa Jeon <aeoragy@gmail.com>
2023-05-11 17:22:57 +0900

    add : Swagger Setting
    Https 스웨거 페이지 CORS 이슈 대응

--- backend/Arcana/src/main/java/com/jumzip/arcana/ArcanaApplication.java
+++ backend/Arcana/src/main/java/com/jumzip/arcana/ArcanaApplication.java
@@ -3,6 +3,7 @@ package com.jumzip.arcana;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://k8d107.p.ssafy.io", description = "Default Server URL")})
 @SpringBootApplication
 public class ArcanaApplication {
 
