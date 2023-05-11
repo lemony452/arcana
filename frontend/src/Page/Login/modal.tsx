69886e0c309caabd3c95eb5d73c0a2b5fe75d66e
강인주 <gij452@kakao.com>
2023-05-11 14:58:04 +0900

    fix : google 로그인 로직 수정

--- frontend/src/Page/Login/modal.tsx
+++ frontend/src/Page/Login/modal.tsx
@@ -2,9 +2,11 @@ import React, { useRef, useEffect } from 'react';
 import { LoginBg, LoginModalStyle } from './login_style';
 import LoginBgImg from '../../Assets/etc/loginBg.png';
 import { GoogleLogin, KakaoLogin } from './login';
+import { userInfoStore } from '../../Store/User/info';
 
 function LoginModal({ goLogin, setGoLogin }: { goLogin: any; setGoLogin: any }) {
   const outside = useRef<any>(null);
+  const { isLogin } = userInfoStore();
 
   const handleClose = (event: any) => {
     if (!outside.current.contains(event.target)) {
@@ -20,7 +22,7 @@ function LoginModal({ goLogin, setGoLogin }: { goLogin: any; setGoLogin: any }) 
   });
 
   return (
-    <LoginModalStyle ref={outside} className={goLogin ? 'gologin' : ''}>
+    <LoginModalStyle ref={outside} className={goLogin && !isLogin ? 'gologin' : ''}>
       <LoginBg src={LoginBgImg} />
       <KakaoLogin />
       <GoogleLogin />
