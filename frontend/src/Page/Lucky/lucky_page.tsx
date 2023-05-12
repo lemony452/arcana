56c24ef1fdec1507b042be1a058187ee732b741b
강인주 <gij452@kakao.com>
2023-05-12 10:52:00 +0900

    fix : 행운 카드 멘트 변경

--- frontend/src/Page/Lucky/lucky_page.tsx
+++ frontend/src/Page/Lucky/lucky_page.tsx
@@ -36,7 +36,7 @@ function LuckyPage() {
       front: F0,
       num: lucky.card.idx,
       title: lucky.card.name,
-      content: `너의 행운 카드는 ${lucky.card.name} 이야! 아마 너에게 특별한 행운을 가져다 줄걸?`,
+      content: `행운 카드는 ${lucky.card.name} 이 나왔습니다!\n${lucky.ment}`,
       result: luckyMent,
     },
     {
@@ -44,7 +44,7 @@ function LuckyPage() {
       front: F0,
       num: lucky.card.idx,
       title: lucky.card.name,
-      content: `너의 행운 카드는 ${lucky.card.name} 이야! 아마 너에게 특별한 행운을 가져다 줄걸?`,
+      content: `행운 카드는 ${lucky.card.name} 이 나왔습니다!\n${lucky.ment}`,
       result: luckyMent,
     },
     {
@@ -52,7 +52,7 @@ function LuckyPage() {
       front: F14,
       num: lucky.card.idx,
       title: lucky.card.name,
-      content: `너의 행운 카드는 ${lucky.card.name} 이야! 아마 너에게 특별한 행운을 가져다 줄걸?`,
+      content: `행운 카드는 ${lucky.card.name} 이 나왔습니다!\n${lucky.ment}`,
       result: luckyMent,
     },
   ];
