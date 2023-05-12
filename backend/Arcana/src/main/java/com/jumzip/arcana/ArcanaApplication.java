ad96a34ba5b370172f88cfa697bec5d9960e929d
jesniey11@gmail.com <jesniey11@gmail.com>
2023-05-12 09:38:56 +0900

    Squashed commit of the following:
    commit e41b84ad6955c29e0163bfee9952a76057db8536
    Author: jesniey11@gmail.com <jesniey11@gmail.com>
    Date:   Fri May 12 09:38:15 2023 +0900
    
        fix : firebaseConfig fileIO error
    
    commit a140d374e2b079f6437afdf1761e210f31a15a63
    Author: jesniey11@gmail.com <jesniey11@gmail.com>
    Date:   Fri May 12 09:05:35 2023 +0900
    
        fix : gitignore
    
    commit d5b9da994076ebaf22f8513c5644b26734e02610
    Merge: 660bb10 2bae8fe
    Author: 전상하 <aeoragy@gmail.com>
    Date:   Thu May 11 17:26:24 2023 +0900
    
        Merge branch 'swaggerSetting' into 'dev-be'
    
        [be]
    
        See merge request s08-final/S08P31D107!49
    
    commit 2bae8fe1cc6bf515d618a431e94e11e89d69fb7e
    Author: SangHa Jeon <aeoragy@gmail.com>
    Date:   Thu May 11 17:26:02 2023 +0900
    
        add : Swagger Setting
    
        누락된 어노테이션 추가
    
    commit 660bb109902995d64f447e381555d93d93e9a348
    Merge: 7bab205 9a1c992
    Author: 전상하 <aeoragy@gmail.com>
    Date:   Thu May 11 17:23:30 2023 +0900
    
        Merge branch 'swaggerSetting' into 'dev-be'
    
        [be]
    
        See merge request s08-final/S08P31D107!48
    
    commit 9a1c992423e2c941a8841f6185b2c1944bad41d1
    Author: SangHa Jeon <aeoragy@gmail.com>
    Date:   Thu May 11 17:22:57 2023 +0900
    
        add : Swagger Setting
    
        Https 스웨거 페이지 CORS 이슈 대응

--- backend/Arcana/src/main/java/com/jumzip/arcana/ArcanaApplication.java
+++ backend/Arcana/src/main/java/com/jumzip/arcana/ArcanaApplication.java
@@ -1,8 +1,11 @@
 package com.jumzip.arcana;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://k8d107.p.ssafy.io", description = "Default Server URL")})
 @SpringBootApplication
 public class ArcanaApplication {
 
