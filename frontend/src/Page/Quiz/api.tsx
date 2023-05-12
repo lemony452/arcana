931b639b10997f057027f19241989699f005058a
Lee_Jungyun <leejy96kr@naver.com>
2023-05-12 09:09:56 +0900

    fix: quiz 시작 페이지 컴포넌트 변경

--- frontend/src/Page/Quiz/api.tsx
+++ frontend/src/Page/Quiz/api.tsx
@@ -1,12 +1,9 @@
 import { shuffleArray } from './random';
 
 export type Question = {
-  category: string;
   correct_answer: string;
-  difficulty: string;
   incorrect_answers: string[];
   question: string;
-  type: string;
 };
 
 export type QuestionsState = Question & { answers: string[] };
