299fdcf23ee3bd437c9c76e0a874452a5e9e472e
lemony452 <gij9808@gmail.com>
2023-05-13 22:17:25 +0900

    add : 마이페이지 사이드바 주간 이용권 & 이벤트 참여 수 항목 추가

--- frontend/src/Page/Mypage/Component/UserInfo.tsx
+++ frontend/src/Page/Mypage/Component/UserInfo.tsx
@@ -1,34 +1,97 @@
 import React, { useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { signOut } from 'firebase/auth';
+import { auth } from '../../../firebase';
 import profile from '../../../Assets/etc/profile.png';
 import cardIcon from '../../../Assets/etc/cardIcon.png';
 import tockenIcon from '../../../Assets/etc/tokenIcon.png';
 import editIcon from '../../../Assets/etc/editIcon.png';
-import { UserContent, Nickname, TarotToken, EditNickName, EditInputBox } from '../mypage_style';
+import logoutIcon from '../../../Assets/etc/logout.png';
+import userIcon from '../../../Assets/etc/user.png';
+import ticketIcon from '../../../Assets/etc/ticket.png';
+import {
+  UserContent,
+  Nickname,
+  TarotToken,
+  EditNickName,
+  ListContent,
+  ListIcon,
+  MoveBtn,
+  MoveBtnImg,
+} from '../mypage_style';
 import { userInfoStore } from '../../../Store/User/info';
+import nextBtn from '../../../Assets/etc/nextButton.png';
 
 function UserInfo() {
-  const { nickname, weeklyCount } = userInfoStore();
+  const { nickname, weeklyCount, setIsLogin, setIsSide, user, ticket } = userInfoStore();
   const navigate = useNavigate();
+
   const MoveEditUser = () => {
     navigate('/editUserInfo');
+  };
+
+  const openTarotList = () => {
+    navigate('/detail');
+  };
+
+  const logout = () => {
+    // logout 시 구글인지 카카오인지 구분해주기
+    if (user.providerId === 'Google') {
+      signOut(auth);
+    }
+    setIsLogin(false);
+    setIsSide(false);
+    console.log('로그아웃');
   };
 
   // const TarotTokenCnt = api
 
   return (
     <UserContent>
-      <img style={{ width: '10vw', height: '10vw' }} src={profile} alt="" />
+      <img style={{ width: '10vw' }} src={profile} alt="" />
       <Nickname>
-        <div>{nickname}</div>
-        <EditNickName onClick={MoveEditUser} src={editIcon} alt="" />
+        <div>{nickname} 님</div>
       </Nickname>
       <TarotToken>
-        <div style={{ display: 'flex', textAlign: 'center', alignItems: 'center' }}>
-          <img style={{ width: '30%', marginRight: '0.3em' }} src={tockenIcon} alt="" />
-          <div>타로 토큰</div>
-        </div>
-        <div>{weeklyCount} 개</div>
+        <ListContent>
+          <ListIcon src={tockenIcon} alt="" />
+          <div>주간 이용권</div>
+        </ListContent>
+        <div style={{ width: '50%' }}>{weeklyCount} / 5</div>
+      </TarotToken>
+      <TarotToken>
+        <ListContent>
+          <ListIcon src={ticketIcon} alt="" />
+          <div>이벤트 참여 횟수</div>
+        </ListContent>
+        <div style={{ width: '50%' }}>{ticket} 회</div>
+      </TarotToken>
+      <TarotToken>
+        <ListContent>
+          <ListIcon src={userIcon} alt="" />
+          <div>내 정보 관리</div>
+        </ListContent>
+        <MoveBtn onClick={MoveEditUser}>
+          <MoveBtnImg src={nextBtn} alt="" />
+        </MoveBtn>
+      </TarotToken>
+      <TarotToken>
+        <ListContent>
+          <ListIcon src={cardIcon} alt="" />
+          <div>타로 운세 기록</div>
+        </ListContent>
+        <MoveBtn onClick={openTarotList}>
+          <MoveBtnImg src={nextBtn} alt="" />
+        </MoveBtn>
+      </TarotToken>
+      <TarotToken>
+        <ListContent>
+          <ListIcon src={logoutIcon} alt="" />
+          <div>로그아웃</div>
+        </ListContent>
+        <MoveBtn onClick={logout}>
+          <MoveBtnImg src={nextBtn} alt="" />
+        </MoveBtn>
       </TarotToken>
     </UserContent>
   );
--- frontend/src/Page/Mypage/Component/editUser.tsx
+++ frontend/src/Page/Mypage/Component/editUser.tsx
@@ -1,17 +1,42 @@
 import React, { useState, useRef } from 'react';
 import { signOut } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 import { auth } from '../../../firebase';
 import { userInfoStore } from '../../../Store/User/info';
-import { UserContent, Nickname, TarotToken, EditNickName, EditInputBox } from '../mypage_style';
+import {
+  UserContent,
+  Nickname,
+  TarotToken,
+  EditNickName,
+  EditInputBox,
+  SideContent,
+  Side,
+  DetailContent,
+  DetailCover,
+  DetailTitle,
+  MoveBtn,
+  DetailFortune,
+  EditUserContent,
+  EditUser,
+  MoveBtnImg,
+} from '../mypage_style';
+import LoveCoverImg from '../../../Assets/etc/cover1.png';
 import editIcon from '../../../Assets/etc/editIcon.png';
+import homeBtnImg from '../../../Assets/etc/homBtn.png';
 import { API } from '../../../API';
+import UserInfo from './UserInfo';
 
 function EditUserInfo() {
   const [edit, setEdit] = useState(false);
   const editRef = useRef<HTMLInputElement>(null);
-  const { nickname, setNickname, user, setIsLogin, setIsSide } = userInfoStore();
+  const { nickname, setNickname, user, setIsLogin, setIsSide, ticket } = userInfoStore();
   const editNickname = () => {
     setEdit(true);
+  };
+
+  const navigate = useNavigate();
+  const MoveMain = () => {
+    navigate('/');
   };
 
   const saveNickName = (e: React.FormEvent<HTMLFormElement>) => {
@@ -19,6 +44,11 @@ function EditUserInfo() {
     const name = editRef.current?.value;
     setNickname(name!);
     setEdit(false);
+    API.put(`api/v1/user/nickname?nickname=${name}`, {
+      headers: {
+        uid: user.uid,
+      },
+    });
   };
 
   const removeUser = async () => {
@@ -28,7 +58,7 @@ function EditUserInfo() {
       },
     });
     // logout 시 구글인지 카카오인지 구분해주기
-    if (user.providerId === 'google.com') {
+    if (user.providerId === 'Google') {
       signOut(auth);
     }
     setIsLogin(false);
@@ -37,21 +67,49 @@ function EditUserInfo() {
   };
 
   return (
-    <div>
-      {!edit ? (
-        <Nickname>
-          <div>{nickname}</div>
-          <EditNickName onClick={editNickname} src={editIcon} alt="" />
-        </Nickname>
-      ) : (
-        <form onSubmit={saveNickName}>
-          <EditInputBox type="text" ref={editRef} placeholder="닉네임을 수정해주세요" />
-        </form>
-      )}
-      <div>{user.email}</div>
-      <button type="button" onClick={removeUser}>
-        회원탈퇴
-      </button>
+    <div style={{ position: 'relative' }}>
+      <Side className="open detail">
+        <div style={{ position: 'absolute', left: 'calc(27vw - 4.5em)' }}>
+          <MoveBtn type="button" onClick={MoveMain}>
+            <MoveBtnImg className="home" src={homeBtnImg} alt="" />
+          </MoveBtn>
+        </div>
+        <SideContent>
+          <UserInfo />
+        </SideContent>
+      </Side>
+      <DetailContent>
+        <DetailCover src={LoveCoverImg} />
+        <DetailTitle>내 정보 관리🙎‍♂️</DetailTitle>
+        <DetailFortune>
+          <EditUser>
+            <Nickname className="edit">🆔 닉네임</Nickname>
+            {!edit ? (
+              <div style={{ width: '30vw', display: 'flex', justifyContent: 'space-between' }}>
+                <EditUserContent>{nickname}</EditUserContent>
+                <EditNickName onClick={editNickname} src={editIcon} alt="" />
+              </div>
+            ) : (
+              <EditUserContent>
+                <form onSubmit={saveNickName}>
+                  <EditInputBox type="text" ref={editRef} placeholder="닉네임을 수정해주세요" />
+                </form>
+              </EditUserContent>
+            )}
+          </EditUser>
+          <EditUser>
+            <Nickname className="edit">📧 Email</Nickname>
+            <EditUserContent>{user.email}</EditUserContent>
+          </EditUser>
+          <EditUser>
+            <Nickname className="edit">🎟️ Quiz 이벤트 참여 여부</Nickname>
+            <EditUserContent>참여{ticket}</EditUserContent>
+          </EditUser>
+          <MoveBtn className="edit" type="button" onClick={removeUser}>
+            회원탈퇴
+          </MoveBtn>
+        </DetailFortune>
+      </DetailContent>
     </div>
   );
 }
