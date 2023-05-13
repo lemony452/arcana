6e96a12904ebcb2496bfeb0fabe09d3acd71aad5
강인주 <gij452@kakao.com>
2023-05-13 23:21:06 +0900

    feat : celtic 운세 결과 저장 api 추가

--- frontend/src/Page/Lucky/lucky_page.tsx
+++ frontend/src/Page/Lucky/lucky_page.tsx
@@ -19,6 +19,17 @@ import KakaoIcon from '../../Assets/etc/icon-kakao.png';
 import Camera from '../../Assets/etc/camera.png';
 import Home from '../../Assets/etc/home.png';
 import { useLuckyStore } from '../../Store/User/lucky';
+import { API } from '../../API';
+import { userInfoStore } from '../../Store/User/info';
+import { useFortuneStore } from '../../Store/User/fortune';
+
+interface SaveDataType {
+  uid: string;
+  options: string;
+  summary: string;
+  question: string;
+  reports: { cardIdx: number; ment: string }[];
+}
 
 function LuckyPage() {
   const navigate = useNavigate();
@@ -26,6 +37,30 @@ function LuckyPage() {
   const [resultPage, setResultPage] = useState(false);
   const [checkSelectState, setCheckSelectState] = useState(false); // 마지막 선택 질문
   const { luckyMent, lucky } = useLuckyStore();
+  const { user } = userInfoStore();
+  const { option, summary, tarotList, fortune, inputValue } = useFortuneStore();
+  const userSummary = summary;
+
+  // 운세 결과 저장 요청 보낼 변수
+  const SaveData: SaveDataType = {
+    uid: user.uid,
+    options: option,
+    summary: userSummary,
+    question: inputValue,
+    reports: [],
+  };
+  // 배열에 타로 운세 결과 저장
+  tarotList.forEach((value, idx) => {
+    SaveData.reports.push({
+      cardIdx: value.card.idx,
+      ment: fortune[idx],
+    });
+  });
+  // 럭키 카드 배열 마지막 요소로 저장
+  SaveData.reports.push({
+    cardIdx: lucky.card.idx,
+    ment: luckyMent,
+  });
 
   const cardList = [
     {
@@ -114,7 +149,21 @@ function LuckyPage() {
     });
   };
 
+  // 럭키카드 공유 후 메인으로 이동하면서 운세기록 저장 api 요청보내기
   const goMain = () => {
+    API.post(`/api/v1/tarot/log`, {
+      uid: SaveData.uid,
+      options: SaveData.options,
+      summary: SaveData.summary,
+      question: SaveData.question,
+      reports: SaveData.reports,
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
     navigate('/');
   };
 
