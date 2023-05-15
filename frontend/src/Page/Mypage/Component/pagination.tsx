a80c57f705edc335724db8aa33d344ba7cd2aaca
lemony452 <gij9808@gmail.com>
2023-05-15 14:39:58 +0900

    add : 타로 운세 기록 페이지 옵션 로직 구현중

--- frontend/src/Page/Mypage/Component/pagination.tsx
+++ frontend/src/Page/Mypage/Component/pagination.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { PaginationContent, PaginationBtn } from '../mypage_style';
 
 function Pagination({ totalPage, pageNum, setPage }: { totalPage: number; pageNum: number; setPage: any }) {
+  const arr = Array(totalPage)
+    .fill(1)
+    .map((x, y) => x + y);
   return (
     <PaginationContent>
-      {Array(totalPage)
-        .fill(0)
-        .map((_, idx) => (
-          <PaginationBtn type="button" onClick={() => setPage(idx + 1)}>
-            {idx + 1}
-          </PaginationBtn>
-        ))}
+      {arr.map((value, idx) => (
+        <PaginationBtn key={value} type="button" onClick={() => setPage(idx + 1)}>
+          {idx + 1}
+        </PaginationBtn>
+      ))}
     </PaginationContent>
   );
 }
