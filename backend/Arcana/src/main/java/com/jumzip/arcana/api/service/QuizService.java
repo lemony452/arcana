b5b629db9a19fd46312bd1e38777004c5d6aadde
SangHa Jeon <aeoragy@gmail.com>
2023-05-15 14:00:32 +0900

    add : Update User Ticket
    퀴즈 보상을 유저에게 지급하는 기능 추가

--- backend/Arcana/src/main/java/com/jumzip/arcana/api/service/QuizService.java
+++ backend/Arcana/src/main/java/com/jumzip/arcana/api/service/QuizService.java
@@ -1,12 +1,15 @@
 package com.jumzip.arcana.api.service;
 
+import com.jumzip.arcana.api.request.QuizRequest;
 import com.jumzip.arcana.db.entity.Quiz;
 import com.jumzip.arcana.db.entity.QuizList;
+import com.jumzip.arcana.db.entity.User;
 
 public interface QuizService {
 
     Quiz getQuiz(int quizNum);
     QuizList getQuizList();
     QuizList resetQuizList(int quizNum);
+    User updateUserTicket(QuizRequest quizRequest);
 
 }
