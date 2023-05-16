226c9b1136a7308418cc8f5f5211b3e08cc7e5eb
lemony452 <gij9808@gmail.com>
2023-05-17 05:27:41 +0900

    add : 웹소켓 test 진행중

---
+++ frontend/src/Page/Quiz/Websocket/testProvider.tsx
@@ -0,0 +1,37 @@
+import React, { useRef, useState } from 'react';
+import { CompatClient, Stomp } from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
+
+export default function ConnectAndSub() {
+  const client = useRef<CompatClient | null>();
+  const [message, setMessage] = useState();
+
+  const connectWs = () => {
+    client.current = Stomp.over(() => {
+      const sock = new SockJS(`https://k8d107.p.ssafy.io:18081/ws/websocket`);
+      console.log(sock);
+      return sock;
+    });
+
+    client.current.connect(
+      // 유효성 검증 header
+      //   {
+      //   Authorization: token,
+      // },
+      {},
+      () => {
+        // callback 함숭 대부분 header 씀
+        client.current!.subscribe(`/sub/channel/quiz`, (mg) => {
+          setMessage(JSON.parse(mg.body));
+          console.log(message);
+        });
+      },
+    );
+  };
+
+  return (
+    <button type="button" onClick={connectWs}>
+      웹소켓 연결 및 구독하기
+    </button>
+  );
+}
