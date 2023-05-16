7236af204e7724ad1d954ce1e8146746a282c646
강인주 <gij452@kakao.com>
2023-05-16 09:55:39 +0900

    add : 주간이용권 & 이벤트 티켓 설명서 추가

--- frontend/src/Page/Mypage/mypage_style.tsx
+++ frontend/src/Page/Mypage/mypage_style.tsx
@@ -34,6 +34,17 @@ export const UserContent = styled.div`
   margin-top: 15vh;
 `;
 
+export const ProfileImg = styled.div`
+  display: flex;
+  width: 10vw;
+  height: 10vw;
+  border-radius: 1em;
+  background: #f3e368;
+  justify-content: center;
+  align-items: center;
+  font-size: 3em;
+`;
+
 export const Nickname = styled.div`
   display: flex;
   font-size: 1.2em;
@@ -235,4 +246,71 @@ export const PaginationBtn = styled.button`
   font-size: 1.5em;
   cursor: pointer;
   /* margin-right: 0.5em; */
+`;
+
+export const HelpIcon = styled.img`
+  width: 1em;
+  height: 1em;
+`;
+
+export const Container = styled.div`
+  position: relative;
+  width: fit-content;
+  height: fit-content;
+  display: inline-block;
+
+  &:hover > .tooltip,
+  &:active > .tooltip {
+    display: block;
+  }
+
+  .tooltip {
+    white-space: pre-line;
+    display: none;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    background-color: #eef3fd;
+    border: #7689fd solid 1px;
+    border-radius: 5px;
+    color: #505bf0;
+    font-size: 12px;
+    font-weight: 500;
+    height: auto;
+    letter-spacing: -0.25px;
+    margin-top: 6.8px;
+    padding: 5px 11px;
+    width: max-content;
+    z-index: 100;
+    transform: translate(-44%, 110%);
+  }
+
+  // 말풍선 테두리와 꼬리를 위한 before, after
+  .tooltip::after {
+    border-color: #eef3fd transparent;
+    border-style: solid;
+    border-width: 0 6px 8px 6.5px;
+    content: '';
+    display: block;
+    left: 50%;
+    transform: translateX(-50%);
+    position: absolute;
+    top: -7px;
+    width: 0;
+    z-index: 1;
+  }
+
+  .tooltip::before {
+    border-color: #7689fd transparent;
+    border-style: solid;
+    border-width: 0 6px 8px 6.5px;
+    content: '';
+    display: block;
+    left: 50%;
+    transform: translateX(-50%);
+    position: absolute;
+    top: -8px;
+    width: 0;
+    z-index: 0;
+  }
 `;
