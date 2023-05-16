d0f417af1726dea919d69c07d75193864a4722dd
Lee_Jungyun <leejy96kr@naver.com>
2023-05-16 22:36:45 +0900

    fix: stomp sockjs 적용

--- frontend/src/Page/Quiz/Websocket/test_one.tsx
+++ frontend/src/Page/Quiz/Websocket/test_one.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import WebSocketProvider from './web_socket_provider';
 import Chatting from './chatting';
-import TextInputBox from './text_input_box';
+// import TextInputBox from './text_input_box';
 
 function Socket() {
   return (
     <div className="Socket">
       <WebSocketProvider>
         <Chatting />
-        <TextInputBox />
+        {/* <TextInputBox /> */}
       </WebSocketProvider>
     </div>
   );
