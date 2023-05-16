bc1d97f9f4298d6a3affe8553bee78c1374ecb72
Lee_Jungyun <leejy96kr@naver.com>
2023-05-16 19:38:47 +0900

    chore: stomp package제거

--- frontend/src/Page/Quiz/Websocket/server.tsx
+++ frontend/src/Page/Quiz/Websocket/server.tsx
@@ -1,15 +1,97 @@
-import { Client } from '@stomp/stompjs';
+import React, { Component } from 'react';
+import SockJsClient from 'react-stomp';
 
-import { WebSocket } from 'ws';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: false,
+      messageText: '',
+      userName: null,
+      messages: [],
+      loginFormInvalid: true,
+    };
+  }
 
-Object.assign(global, { WebSocket });
+  handleLogin = () => {
+    if (this.state.userName) {
+      this.setState({ loggedIn: true });
+      this.clientRef.sendMessage('/app/chat.addUser', JSON.stringify({ sender: this.state.userName, type: 'JOIN' }));
+    } else {
+      this.setState({ loginFormInvalid: true });
+    }
+  };
 
-const client = new Client({
-  brokerURL: 'ws://localhost:15674/ws',
-  onConnect: () => {
-    client.subscribe('/topic/test01', (message) => console.log(`Received: ${message.body}`));
-    client.publish({ destination: '/topic/test01', body: 'First Message' });
-  },
-});
+  handleLogout = () => {
+    this.setState({
+      loggedIn: false,
+      messages: [],
+    });
+    this.clientRef.disconnect();
+    this.clientRef.connect();
+  };
 
-client.activate();
+  handleMessageSend = () => {
+    if (this.state.userName) {
+      this.clientRef.sendMessage(
+        '/app/chat.sendMessage',
+        JSON.stringify({ sender: this.state.userName, content: this.state.messageText, type: 'CHAT' }),
+      );
+      this.setState({ messageText: '' });
+    }
+  };
+
+  handleChange = (name) => (event) => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
+
+  handleChangeUserName = () => (event) => {
+    this.setState({
+      userName: event.target.value,
+      loginFormInvalid: event.target.value.length < 3,
+    });
+  };
+
+  handleMessageReceived = (msg) => {
+    this.setState({ messages: this.state.messages.concat(msg) });
+  };
+
+  render() {
+    const visibleLoginForm = !this.state.loggedIn;
+    const visibleLogout = this.state.loggedIn;
+
+    return (
+      <>
+        <CssBaseline />
+        <SockJsClient
+          url="http://localhost:3000/ws"
+          topics={['/topic/public']}
+          onMessage={(msg) => this.handleMessageReceived(msg)}
+          ref={(client) => {
+            this.clientRef = client;
+          }}
+        />
+        <Header onLogout={() => this.handleLogout()} showLogout={visibleLogout} />
+        <LoginForm
+          visible={visibleLoginForm}
+          loginFornInvalid={this.state.loginFormInvalid}
+          onLogin={() => this.handleLogin()}
+          onChange={() => this.handleChangeUserName()}
+        >
+          {' '}
+        </LoginForm>
+        <Chat
+          visible={!visibleLoginForm}
+          messages={this.state.messages}
+          messageText={this.state.messageText}
+          onChange={() => this.handleChange('messageText')}
+          onSendMessage={() => this.handleMessageSend()}
+        />
+      </>
+    );
+  }
+}
+
+export default App;
