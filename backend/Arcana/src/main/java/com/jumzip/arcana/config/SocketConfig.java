4b61eaf76e5981d4301bf07941d7ecd33a531c0f
jesniey11@gmail.com <jesniey11@gmail.com>
2023-05-17 09:25:10 +0900

    test : socket test

--- backend/Arcana/src/main/java/com/jumzip/arcana/config/SocketConfig.java
+++ backend/Arcana/src/main/java/com/jumzip/arcana/config/SocketConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class SocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
     }
 
     @Override
