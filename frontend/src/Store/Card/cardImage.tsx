a6e569158f1e92fd2b32378f4b6574cd9b8bfc29
lemony452 <gij9808@gmail.com>
2023-05-18 13:41:44 +0900

    폴더 일괄 삭제

--- frontend/src/Store/Card/cardImage.tsx
+++
@@ -1,6 +0,0 @@
-import { create } from 'zustand';
-
-interface ImageDataList {
-  indexList: number[];
-  setIndexList: (indexList: number[]) => void;
-}
--- frontend/src/setupProxy.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = (app) => {
-  app.use('/ws', createProxyMiddleware({ target: 'https://k8d107.p.ssafy.io:18081', ws: true }));
-};
